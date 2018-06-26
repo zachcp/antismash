688f7bd8c4cc6c9df616f92a5f6a6930134efcfe
zach charlop-powers <zach.charlop.powers@gmail.com>
2018-06-26 11:24:27 -0400

    add base dir as a package to include the run_antismash.py script

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ class PyTest(TestCommand):
 setup(
     name="antismash",
     version=read_version(),
-    packages=['antismash'],
+    packages=['antismash', '.'],
     author='antiSMASH development team',
     author_email='antismash@secondarymetabolites.org',
     description='The antibiotics and Secondary Metabolites Analysis Shell.',
